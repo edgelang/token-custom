5ef71e6e8373b55a37f6b8cb83481f8d28f08519
Alex <china.alex.zhao@gmail.com>
2020-12-13 23:13:36 +0800

    last in first out for locked token transfer

--- contracts/libraries/TokenUtility.sol
+++ contracts/libraries/TokenUtility.sol
@@ -56,16 +56,17 @@ library TokenUtility{
     /**
      * @dev cost amount of token among balanceFreeTime Keys indexed in records with recordCostRecords
      * return cost keys and cost values one to one 
+     * LIFO
      */
     function calculateCostLocked(mapping (uint => uint256) storage records,uint256 toCost,uint[] memory keys,mapping (uint => uint256) storage recordsCost)internal view returns(uint256,uint256[] memory){
         uint256 lockedFreeToMove = 0;
         uint256[] memory cost = new uint256[](keys.length);
-        for (uint256 ii=0; ii < keys.length; ++ii){
+        for (uint256 ii=keys.length; ii > 0; --ii){
             //_lockTimeUnitPerSeconds:days:25*7,rounds:25
             if (toCost==0){
                 break;
             }
-            uint freeTime = keys[ii];
+            uint freeTime = keys[ii-1];
             uint256 lockedBal = records[freeTime];
             uint256 alreadyCost = recordsCost[freeTime];
             
@@ -73,10 +74,10 @@ library TokenUtility{
 
             lockedFreeToMove = lockedFreeToMove.add(lockedToMove);
             if (lockedToMove >= toCost){
-                cost[ii] = toCost;
+                cost[ii-1] = toCost;
                 toCost = 0;
             }else{
-                cost[ii] = lockedToMove;
+                cost[ii-1] = lockedToMove;
                 toCost = toCost.sub(lockedToMove,"lockedToMove>toCost");
             }
         }
