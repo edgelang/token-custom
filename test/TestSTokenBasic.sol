771bf431b3ab477c26876c9067f61b4df0ab5624
Alex <china.alex.zhao@gmail.com>
2020-12-13 23:17:31 +0800

    add unit test for Standardhashrate token

---
+++ test/TestSTokenBasic.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/StandardHashrateToken.sol";
+import "../contracts/BTCST.sol";
+
+contract TestSToken{
+
+    
+}
