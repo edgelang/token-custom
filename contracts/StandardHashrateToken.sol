0817f4f1eb6fa27ad2549b8b44e5d816e5033986
Alex <china.alex.zhao@gmail.com>
2020-12-16 02:18:34 +0800

    fix imports

--- contracts/StandardHashrateToken.sol
+++ contracts/StandardHashrateToken.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "./libraries/LinearReleaseToken.sol";
 
 contract StandardHashrateToken is LinearReleaseToken{
