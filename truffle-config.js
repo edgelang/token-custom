f69b1fda104046409af189e9ed724e846f2a0031
Alex <china.alex.zhao@gmail.com>
2020-12-16 03:19:42 +0800

    add bsc testnet and bsc chain

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync("./console/secret").toString().trim();
 
 module.exports = {
   /**
@@ -47,6 +47,23 @@ module.exports = {
      //7545,9545
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+    testbsc:{
+      networkCheckTimeout:100000,
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-2-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc:{
+      networkCheckTimeout:100000,
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      production: true
     }
     // Another network with more advanced options...
     // advanced: {
